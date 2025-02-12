d4fc9355b1709947db77a562b2da896cf1ed5402
AbdulCodeCraft <ar.abdulrahman.a@gmail.com>
2025-02-12 18:43:46 +0530

    Static component and styles created

--- my_app/src/Components/QrCode.jsx
+++ my_app/src/Components/QrCode.jsx
@@ -3,8 +3,21 @@ import '../css/QrCode.css';
 
 const QrCode = () => {
   return (
-    <div>
-      This is the QR Component
+    <div className='App-Container'>
+        <img src='https://placehold.co/150
+'/>
+      <div>
+        <label htmlFor='dataInput' className='Input-label'>
+            Data for QR code:
+        </label>
+        <input type='text' id='dataInput' placeholder='Enter data for QR code'/>
+        <label htmlFor='sizeInput' className='Input-label'>
+            Image size (e.g., 150)
+        </label>
+        <input type='text' id='sizeInput' placeholder='Enter image size'/>
+        <button>Generate QR code</button>
+        <button>Download QR code</button>
+      </div>
     </div>
   )
 }
